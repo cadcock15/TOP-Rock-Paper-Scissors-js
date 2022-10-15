6a2bf141242b50c353e8aa5b83995e5c52104fec
cadcock15 <5466552+cadcock15@users.noreply.github.com>
2022-10-15 11:49:20 -0700

    Made playRound function with nested switch. Add prompt for user input with error checking loop.

--- game.js
+++ game.js
@@ -1,11 +1,7 @@
 
-const buttons = document.querySelectorAll('.btn');
-let playerSelection = "rock";
-buttons.forEach((button)=>{button.addEventListener('click',()=>{
-    game();
-    })
-})
 
+
+game();
 
 
 function getComputerChoice() {
@@ -21,11 +17,60 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection.toLowerCase == computerSelection) {
+        return ("Tie! You both chose " + playerSelection);
+    } else {
+        switch(playerSelection) {
+            case "rock":
+                switch(computerSelection) {
+                    case "rock":
+                        return "Tie! You both chose rock.";
+                    case "paper":
+                        return "You lose! Rock is covered by paper.";
+                    case "scissors":
+                        return "You Win! Rock crushes scissors.";
+                }
+                return "oops";
+            case "paper":
+                switch(computerSelection) {
+                    case "rock":
+                        return "You Win! Paper covers rock.";
+                    case "paper":
+                        return "Tie! You both chose paper.";
+                    case "scissors":
+                        return "You lose! paper is cut by scissors.";
+                }
+            case "scissors":
+                switch(computerSelection) {
+                    case "rock":
+                        return "You lose! Scissors is crushed by rock.";
+                    case "paper":
+                        return "You win! Scissors cuts paper.";
+                    case "scissors":
+                        return "Tie! You both chose scissors.";
+                }
+        }
+    }
 }
 
 function game() {
+    let valid = false;
+    while(valid == false) {
+        playerChoice = prompt();
+        switch(playerChoice.toLowerCase()) {
+        case "rock":
+        case "paper":
+        case "scissors":
+            valid = true;
+            break;
+        default:
+            console.log("Error: Please enter rock, paper, or scissors.");
+        }
+    }
+    
     let compChoice = getComputerChoice();
-    console.log("computer: " + compChoice);
+    console.log("player: " + playerChoice +",computer: " + compChoice);
+    console.log(playRound(playerChoice, compChoice));
 }
 
