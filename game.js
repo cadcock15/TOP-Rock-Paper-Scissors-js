c052ae65f7ad2235334e2dc6b55b6c2056ebfd2f
cadcock15 <5466552+cadcock15@users.noreply.github.com>
2022-10-15 12:35:51 -0700

    Add score tracking with global variables Add game loop until 5 wins

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+let playerScore = 0;
+let compScore = 0;
 
 
 
@@ -18,59 +20,78 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
-    if (playerSelection.toLowerCase == computerSelection) {
-        return ("Tie! You both chose " + playerSelection);
-    } else {
-        switch(playerSelection) {
-            case "rock":
-                switch(computerSelection) {
-                    case "rock":
-                        return "Tie! You both chose rock.";
-                    case "paper":
-                        return "You lose! Rock is covered by paper.";
-                    case "scissors":
-                        return "You Win! Rock crushes scissors.";
-                }
-                return "oops";
-            case "paper":
-                switch(computerSelection) {
-                    case "rock":
-                        return "You Win! Paper covers rock.";
-                    case "paper":
-                        return "Tie! You both chose paper.";
-                    case "scissors":
-                        return "You lose! paper is cut by scissors.";
-                }
-            case "scissors":
-                switch(computerSelection) {
-                    case "rock":
-                        return "You lose! Scissors is crushed by rock.";
-                    case "paper":
-                        return "You win! Scissors cuts paper.";
-                    case "scissors":
-                        return "Tie! You both chose scissors.";
-                }
-        }
+    switch(playerSelection) {
+        case "rock":
+            switch(computerSelection) {
+                case "rock":
+                    return "Tie! You both chose rock.";
+                case "paper":
+                    compScore++;
+                    return "You lose! Rock is covered by paper.";
+                case "scissors":
+                    playerScore++;
+                    return "You Win! Rock crushes scissors.";
+            }
+            return "oops";
+        case "paper":
+            switch(computerSelection) {
+                case "rock":
+                    playerScore++;
+                    return "You Win! Paper covers rock.";
+                case "paper":
+                    return "Tie! You both chose paper.";
+                case "scissors":
+                    compScore++;
+                    return "You lose! paper is cut by scissors.";
+            }
+        case "scissors":
+            switch(computerSelection) {
+                case "rock":
+                    compScore++;
+                    return "You lose! Scissors is crushed by rock.";
+                case "paper":
+                    playerScore++;
+                    return "You win! Scissors cuts paper.";
+                case "scissors":
+                    return "Tie! You both chose scissors.";
+            }
     }
 }
 
 function game() {
-    let valid = false;
-    while(valid == false) {
-        playerChoice = prompt();
-        switch(playerChoice.toLowerCase()) {
-        case "rock":
-        case "paper":
-        case "scissors":
-            valid = true;
-            break;
-        default:
-            console.log("Error: Please enter rock, paper, or scissors.");
+
+    console.log("Welcome to a game of Rock Paper Scissors.");
+    console.log("You will be versing a computer. First to 5 wins!");
+
+
+    while(playerScore != 5 && compScore != 5) {
+        let valid = false;
+        while(valid == false) {
+            const playerChoice = prompt();
+            switch(playerChoice.toLowerCase()) {
+            case "rock":
+            case "paper":
+            case "scissors":
+                valid = true;
+                break;
+            default:
+                console.log("Error: Please enter rock, paper, or scissors.");
+            }
         }
+        
+        const compChoice = getComputerChoice();
+        console.log("player: " + playerChoice +",computer: " + compChoice);
+        console.log(playRound(playerChoice, compChoice));
+    }
+    if(playerScore == 5) {
+        console.log("You Won the game! Congratulations!");
+    } else {
+        console.log("You Lost the game. Better luck next time.");
     }
     
-    let compChoice = getComputerChoice();
-    console.log("player: " + playerChoice +",computer: " + compChoice);
-    console.log(playRound(playerChoice, compChoice));
+    
+
+
+    
 }
 
