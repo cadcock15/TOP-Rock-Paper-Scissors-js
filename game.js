af72c9b2a2b7cacab03f849a947b04c30b5387b5
cadcock15 <5466552+cadcock15@users.noreply.github.com>
2022-10-21 20:19:28 -0700

    ADD SVG and modify it ADD SVG highlight events

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 let playerScore = 0;
 let compScore = 0;
 
-game();
+//game();
 
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 3);
@@ -59,7 +59,7 @@ function game() {
     console.log("Welcome to a game of Rock Paper Scissors.");
     console.log("You will be versing a computer. First to 5 wins!");
 
-    while(playerScore != 5 && compScore != 5) {
+    //while(playerScore != 5 && compScore != 5) {
         let valid = false;
         while(valid == false) {
             var playerChoice = prompt();
@@ -76,11 +76,83 @@ function game() {
         var compChoice = getComputerChoice();
         console.log("player: " + playerChoice +",computer: " + compChoice);
         console.log(playRound(playerChoice, compChoice));
-    }
-    if(playerScore == 5) {
-        console.log("You Won the game! Congratulations!");
-    } else {
-        console.log("You Lost the game. Better luck next time.");
-    }
+    // }
+    // if(playerScore == 5) {
+    //     console.log("You Won the game! Congratulations!");
+    // } else {
+    //     console.log("You Lost the game. Better luck next time.");
+    // }
 }
 
+//Selectors
+const rockVector = document.querySelector('#g2982');
+const paperVector = document.querySelector('#g2906');
+const scissorsVector = document.querySelector('#g3028');
+
+//Event listeners
+const buttons = document.querySelectorAll('.buttons > button');//selects all button child elements in class buttons
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        //console.log(button.id);//debug
+        console.log(playRound(button.id,getComputerChoice()));
+    });
+});
+
+rockVector.addEventListener('mouseover', () => {
+    rockVector.style.fill = 'blue';
+    rockVector.style.stroke = 'darkBlue';
+    rockVector.style.strokeWidth = '4px';
+});
+
+rockVector.addEventListener('mouseout', () => {
+    rockVector.style.fill = 'grey';
+    rockVector.style.stroke = 'black';
+    rockVector.style.strokeWidth = '2px';
+})
+
+scissorsVector.addEventListener('mouseover', () => {
+    scissorsVector.style.fill = 'red';
+    scissorsVector.style.stroke = 'darkRed';
+    scissorsVector.style.strokeWidth = '2px';
+});
+
+scissorsVector.addEventListener('mouseout', ()=> {
+    scissorsVector.style.fill = 'grey';
+    scissorsVector.style.stroke = 'black';
+    scissorsVector.style.strokeWidth = '1px';
+});
+
+paperVector.addEventListener('mouseover', ()=> {
+    paperVector.style.fill = 'green';
+    paperVector.style.stroke = 'darkGreen';
+    paperVector.style.strokeWidth = '2px';
+});
+
+paperVector.addEventListener('mouseout', ()=> {
+    paperVector.style.fill = 'grey';
+    paperVector.style.stroke = 'black';
+    paperVector.style.strokeWidth = '1px';
+});
+
+
+
+//Javascript DOM manipulation
+
+//have the hands enlarge slightly on hover
+//could have the actual svg's transform position to appear that they are attacking the loser
+
+//make the actual svg's the buttons
+//make the user selection blue
+//make the computer selection green
+//make a tie red
+
+rockVector.style.fill = 'grey';
+rockVector.style.stroke = 'black';
+rockVector.style.strokeWidth = '2px';
+
+
+paperVector.style.fill = 'grey';
+paperVector.style.stroke = 'black';
+
+scissorsVector.style.fill = 'grey';
+scissorsVector.style.stroke = 'black';
